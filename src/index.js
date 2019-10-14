f60c3fc7c38912622cb97767a997edb664ba85b1
Carlson Kamau <carlsonkamau@gmail.com>
2019-10-14 12:31:50 +0300

    state and changing state

--- src/index.js
+++ src/index.js
@@ -20,15 +20,54 @@ const Book = ({title, author , pages}) => {
     )
 }
 
-const Library = ({books}) => {
-    return (
-        <div>
-            {books.map(
-               books => <Book title = {books.title} author ={books.author} pages ={books.pages}/>
-            )}
-           
-        </div>
-    )
+class Library extends React.Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            open: false
+        }
+        this.toggleOpenClosed = this.toggleOpenClosed.bind(this)
+        
+    }
+
+    //you can change this to make more sense if you want to use 
+    // previous state
+    // what to use ?
+    //         toggleOpenClosed(){
+    //              this.setState(prevState =>({
+    //          open: !prevState.open
+    //         }))
+    //  }
+    
+
+    toggleOpenClosed(){
+        this.setState({
+            open: !this.state.open
+        })
+    }
+    
+    render(){
+        
+        const {books} = this.props
+        return (
+            <div>
+                <h1>The library is {this.state.open ? 'open': 'closed'}</h1>
+                <button onClick = {this.toggleOpenClosed}>Change</button>
+                {books.map(
+                   (books, i) => 
+                   <Book 
+                   key = {i}
+                   title = {books.title} 
+                   author ={books.author} 
+                   pages ={books.pages}/>
+                )}
+
+               
+            </div>
+        )
+
+    }
+   
 }
 
 
