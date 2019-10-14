760b101423a9aded04a3c248e9513de2a3f15b1d
Carlson Kamau <carlsonkamau@gmail.com>
2019-10-14 10:52:40 +0300

    14/10/19

--- src/index.js
+++ src/index.js
@@ -1,45 +1,41 @@
-import React, {Component} from 'react';
+import React from 'react' ;
 import {render} from 'react-dom';
 
-let skiData = {
-    total:50,
-    powder: 20,
-    backcountry: 10,
-    goal:100
-}
 
+let bookList = [
+    {"title" : "When the sun Shines ", "author":"James brown", "pages": 238},
+    {"title" : " The River Between ", "author":"Ngugi wa Thiongo", "pages": 400},
+    {"title" : "Damu Nyeusi ", "author":"Kitile ", "pages": 300},
 
-class SkiDayCounter extends Component{
-    render (){
-        const {total, powder , backcountry, goal}= this.props
-        return (
-            <section>
-                <div>
-                    <p> Total Days: {total}</p>
-                </div>
-                <div>
-                    <p> Powder Days: {powder}</p>
-                </div>
-                <div>
-                    <p> Backcountry Days : {backcountry}</p>
-                </div>
-                <div>
-                    <p> Goal: {goal}</p>
-                </div>
-            </section>
-            
-        )
-    }
-}
+]
+const Book = ({title, author , pages}) => {
+    return (
+        <section>
+            <h1>{title}</h1>
+            <p>Author is {author}</p>
+            <p>Pages: {pages}</p>
 
+        </section>
 
-render(
-    <SkiDayCounter
-    total = {skiData.total}
-    powder = {skiData.total}
-    backcountry = {skiData.backcountry}
-    goal = {skiData.goal}/>,
-    document.getElementById('root'
     )
-)
+}
+
+const Library = ({books}) => {
+    return (
+        <div>
+            {books.map(
+               books => <Book title = {books.title} author ={books.author} pages ={books.pages}/>
+            )}
+           
+        </div>
+    )
+}
+
+
+
+    render(
+        <Library books ={bookList}/>,
+        document.getElementById ('root')
+    )
+
 
